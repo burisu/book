97586af6c1ab27f842e1f7821391ee5b620b7a6c
Michel Gil Antoli <michel@gilantoli.com>
2007-12-20 13:03:05 +0000

    git-svn-id: https://www.rotex1690.org/svn@17 418a08b3-2a41-0410-b009-95f9e35b0d20

--- app/models/email.rb
+++ app/models/email.rb
@@ -27,5 +27,59 @@
 #
 
 class Email < ActiveRecord::Base
+  def initialize(*params)
+    super(*params)
+    zail = params[0]
+    self.arrived_at = Time.now
+    self.sent_on = Date.today
+    self.subject = zail.subject
+    self.charset = zail.charset
+    self.header  = 'Headers' #zail.header.collect{|x| x[0]+':"'+x[1]+'"'}.join(",")
+    self.unvalid = false
+    self.from_valid = true
+    self.from_id = " 152 ";
+    self.manual_sent=false
+    
+
+    if zail.from.is_a? String
+      self.from = zail.from
+    else
+      self.from = zail.from.join(',') if zail.from.is_a? Array
+      self.from_valid = false
+      self.unvalid = true;
+    end
+    
+    # Validite de l'adresse de l'expediteur
+    unless self.unvalid?
+      person = Person.find_by_self(self.from)
+      unless person
+        self.unvalid = true
+        self.from_valid = false
+      else
+        self.from_id = person.id
+      end
+    end
+
+    # Validite de l'adresse de destination
+    selfs = []
+    if zail.recipients.is_a? String 
+      self.recipients = zail.recipients
+      selfs << selfs.recipients
+    elsif zail.recipients.is_a? Array
+      for x in zail.recipients
+        if x.match('.*<.*>')
+          selfs << x.gsub(/.*</,'').gsub(/>.*/,'').strip
+        else
+          selfs << x
+        end
+      end
+    else
+      self.unvalid = true
+    end
+      
+    self.recipients = "0123456789"
+
+  
+  end
 
 end
--- app/models/maily.rb
+++ app/models/maily.rb
@@ -13,62 +13,15 @@ class Maily < ActionMailer::Base
     # Verification de l'identite de l'expediteur
     # verification d'adresse unique
 
-    email = Email.new
-    email.arrived_at = Time.now
-    email.sent_on = Date.today
-    email.subject = zail.subject
-    email.charset = zail.charset
-    email.header  = 'Headers' #zail.header.collect{|x| x[0]+':"'+x[1]+'"'}.join(",")
-    email.unvalid = false
-    email.from_valid = true
-    email.from_id = " 152 ";
-    email.manual_sent=false
-    
-
-    if zail.from.is_a? String
-      email.from = zail.from
-    else
-      email.from = zail.from.join(',') if zail.from.is_a? Array
-      email.from_valid = false
-      email.unvalid = true;
-    end
-    
-    # Validite de l'adresse de l'expediteur
-    unless email.unvalid?
-      person = Person.find_by_email(email.from)
-      unless person
-        email.unvalid = true
-        email.from_valid = false
-      else
-        email.from_id = person.id
-      end
-    end
-
-    # Validite de l'adresse de destination
-    emails = []
-    if zail.recipients.is_a? String 
-      email.recipients = zail.recipients
-      emails << emails.recipients
-    elsif zail.recipients.is_a? Array
-      for x in zail.recipients
-        if x.match('.*<.*>')
-          emails << x.gsub(/.*</,'').gsub(/>.*/,'').strip
-        else
-          emails << x
-        end
-      end
-    else
-      email.unvalid = true
-    end
-      
-    email.recipients = "0123456789"
-
-
-    # Expedition du message
+    email = Email.new(zail)
+    email.deliver unless email.unvalid?
     email.save!
     
+    # Expedition du message
+ #   email.save!
+    
 #    fw = Maily.fw(
-    Maily.deliver_fw("brice.texier@fdsea33.fr",email.subject) unless email.unvalid?
+#    Maily.deliver_fw("brice.texier@fdsea33.fr",email.subject) unless email.unvalid?
     
   end
 
