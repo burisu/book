24b09a7370356dfeebc57058f1958cc40e3e795f
Brice Texier <burisu@oneiros.fr>
2008-04-04 15:32:21 +0000

    git-svn-id: https://www.rotex1690.org/svn@37 418a08b3-2a41-0410-b009-95f9e35b0d20

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,16 @@
+require 'mongrel_cluster/recipes'
+
 set :application, "rotex"
 set :repository,  "https://svn.rotex1690.org/rotex"
+
+set :runner, "brice"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
 set :deploy_to, "/var/www/www.rotex1690.org/#{application}"
+
+set :mongrel_conf, "#{current_path}/config/mongrel_cluster.yml"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
