092ba84c056e46ffa2c0f980d54532d8e06455f1
Brice Texier <burisu@oneiros.fr>
2008-04-11 13:43:48 +0000

    git-svn-id: https://www.rotex1690.org/svn@38 418a08b3-2a41-0410-b009-95f9e35b0d20

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :runner, "brice"
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-set :deploy_to, "/var/www/www.rotex1690.org/#{application}"
+set :deploy_to, "/var/www/www.rotex1690.org/"
 
 set :mongrel_conf, "#{current_path}/config/mongrel_cluster.yml"
 
