82b191edae687cd8ab6bd4779fe15f08b94fce02
Brice Texier <burisu@oneiros.fr>
2009-03-12 21:19:29 +0000

    git-svn-id: https://www.rotex1690.org/svn@120 418a08b3-2a41-0410-b009-95f9e35b0d20

--- app/models/zone.rb
+++ app/models/zone.rb
@@ -26,6 +26,7 @@ class Zone < ActiveRecord::Base
     if self.nature and self.nature.parent
       errors.add(:parent_id, "doit être du type \""+self.nature.parent.name+"\" ") if self.parent and self.parent.nature != self.nature.parent
       errors.add(:parent_id, "doit être renseigné") if self.parent.nil? and !self.nature.parent.nil?
+      errors.add(:parent_id, "ne doit pas être renseigné") if !self.parent.nil? and self.nature.parent.nil?
     end
   end
 
