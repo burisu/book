3ce04bd9dc1e438ea724e88ce3f07fbfe820f5e0
Brice Texier <burisu@oneiros.fr>
2009-03-12 22:49:36 +0000

    git-svn-id: https://www.rotex1690.org/svn@126 418a08b3-2a41-0410-b009-95f9e35b0d20

--- app/controllers/intra_controller.rb
+++ app/controllers/intra_controller.rb
@@ -34,10 +34,10 @@ class IntraController < ApplicationController
     @reports = []
     start = @folder.begun_on.at_beginning_of_month
     stop = (Date.today<@folder.finished_on ? Date.today : @folder.finished_on)
-    while start <= stop do
-      article = Article.find(:first, :conditions=>{:done_on=>start, :author_id=>session[:current_person_id]})
-      @reports << {:title=>start.year.to_s+'/'+start.month.to_s+' - '+(article.nil? ? "" : article.title_h), :month=>start.year.to_s+start.month.to_s}
-      start >> 1
+#    while start <= stop do
+#      article = Article.find(:first, :conditions=>{:done_on=>start, :author_id=>session[:current_person_id]})
+#      @reports << {:title=>start.year.to_s+'/'+start.month.to_s+' - '+(article.nil? ? "" : article.title_h), :month=>start.year.to_s+start.month.to_s}
+#      start >> 1
     end
       
   end
