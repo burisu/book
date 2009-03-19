358eb71908cdcc126368dd3db1d0b09eaafc939e
Brice Texier <burisu@oneiros.fr>
2009-03-19 22:50:25 +0000

    git-svn-id: https://www.rotex1690.org/svn@147 418a08b3-2a41-0410-b009-95f9e35b0d20

--- app/controllers/intra_controller.rb
+++ app/controllers/intra_controller.rb
@@ -375,7 +375,11 @@ class IntraController < ApplicationController
 
   def zones_create
     @parent = Zone.find_by_id(params[:id])
-    @natures = ZoneNature.find(:all, :conditions=>@parent ? ["parent_id=?", @parent.nature_id ] : ["parent_id IS NULL"], :order=>:name) if @parent
+    if @parent
+      @natures = ZoneNature.find(:all, :conditions=>["parent_id=?", @parent.nature_id ], :order=>:name) 
+    else
+      @natures = ZoneNature.find(:all, :conditions=>["parent_id IS NULL"], :order=>:name) 
+    end
     @parents = (@parent.nil? ? [] : @parent.parents)
 
     if request.post?
