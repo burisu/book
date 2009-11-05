fcbc29fce082191f6feb67015b295303bcd543bf
Brice Texier <burisu@oneiros.fr>
2009-11-05 23:39:38 +0000

    Fixe promotions name
    git-svn-id: https://www.rotex1690.org/svn@340 418a08b3-2a41-0410-b009-95f9e35b0d20

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -22,6 +22,27 @@
 
 class Folder < ActiveRecord::Base
 
+
+  def before_validation
+    if self.begun_on and self.departure_country
+      from = (self.begun_on.month>=5 ? 'N' : 'S')
+      out = (self.departure_country.iso3166.lower == 'fr' ? true : false)
+      pn = "#{out ? 'Out' : 'In'} #{self.begun_on.year} #{from}"
+      promotion = Promotion.find_by_name(pn)
+      promotion = Promotion.create(:name=>pn, :is_outbound=>out, :from_code=>from) if promotion.nil?
+      self.promotion_id = promotion.id
+    end
+  end
+
+  def validate
+    if self.proposer_zone
+      errors.add(:proposer_zone_id, "ne correspond pas au pays de départ") if self.proposer_zone.country_id!=self.departure_country_id
+    end
+    if self.host_zone
+      errors.add(:host_zone_id, "ne correspond pas au pays d'arrivée") if self.host_zone.country_id!=self.arrival_country_id
+    end
+  end
+
   def reports
     Article.find(:all, :conditions=>["author_id=? AND done_on IS NOT NULL AND natures NOT LIKE '%agenda%'", self.person_id], :order=>:done_on);
   end
