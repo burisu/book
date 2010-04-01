eccfa08e19802c72ff625bd68ab19ab16bae8e0a
Brice Texier <burisu@oneiros.fr>
2010-04-01 21:38:34 +0000

    Add SSL on Suivi
    git-svn-id: https://www.rotex1690.org/svn@415 418a08b3-2a41-0410-b009-95f9e35b0d20

--- app/controllers/suivi_controller.rb
+++ app/controllers/suivi_controller.rb
@@ -1,5 +1,6 @@
 class SuiviController < ApplicationController
   before_filter :authorize, :except=>[:access_denied, :logout]
+  ssl_only
 
   def index
     # @questionnaires = Questionnaire.find(:all, :conditions=>["id IN (SELECT questionnaire_id FROM answers) OR CURRENT_DATE BETWEEN COALESCE(started_on, CURRENT_DATE-'1 day'::INTERVAL) AND COALESCE(stopped_on, CURRENT_DATE-'1 day'::INTERVAL)"])
