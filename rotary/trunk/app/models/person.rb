7900266ebc34b96fec58c08bc7d8953274f7fa09
Brice Texier <burisu@oneiros.fr>
2011-01-05 08:59:45 +0000

    Fixes squeeze bug on rotex email
    git-svn-id: https://www.rotex1690.org/svn@463 418a08b3-2a41-0410-b009-95f9e35b0d20

--- rotary/trunk/app/models/person.rb
+++ rotary/trunk/app/models/person.rb
@@ -74,7 +74,7 @@ class Person < ActiveRecord::Base
     self.family_name = self.patronymic_name if self.family_name.blank?
     self.forced = false if self.forced.nil?
     self.user_name.gsub!(/(-|\.|\ )/, '')
-    self.rotex_email = self.first_name.strip.squeeze.gsub(/\s*\-\s*/, '-').gsub(/\s+/, '_').lower_ascii+'.'+patronymic_name.strip.squeeze.gsub(/\s*\-\s*/, '-').gsub(/\s+/, '_').lower_ascii+'@rotex1690.org'
+    self.rotex_email = self.first_name.to_s.strip.gsub(/\s*\-\s*/, '-').gsub(/\s+/, '_').lower_ascii+'.'+patronymic_name.strip.gsub(/\s*\-\s*/, '-').gsub(/\s+/, '_').lower_ascii+'@rotex1690.org'
 
     self.validation = Person.generate_password(73+2*(10*rand).to_i) unless self.is_validated or !self.replacement_email.blank?
     if self.latitude.blank?
