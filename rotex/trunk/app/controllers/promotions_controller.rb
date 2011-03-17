68117a733850d1b679c3f669efe5a1cef42eee4e
Michel Gil Antoli <michel@gilantoli.com>
2011-03-17 20:19:18 +0000

    retrait dyta promotions_controller
    git-svn-id: https://www.rotex1690.org/svn@522 418a08b3-2a41-0410-b009-95f9e35b0d20

--- rotex/trunk/app/controllers/promotions_controller.rb
+++ rotex/trunk/app/controllers/promotions_controller.rb
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 class PromotionsController < ApplicationController
 
-  dyta(:people, :model=>:people, :order=>"family_name, first_name", :conditions=>"(session[:current_promotion_id].blank? ? {} : {:promotion_id=>session[:current_promotion_id]}).merge(:student=>true)", :line_class=>"(RECORD.current? ? 'notice' : '')", :order=>"started_on") do |t|
-    t.action :folder, :image=>:show
-    t.column :family_name, :url=>{:controller=>:people, :action=>:show}
-    t.column :first_name, :url=>{:controller=>:people, :action=>:show}
-    t.column :code, :through=>:promotion
-    t.column :started_on, :label=>"Du"
-    t.column :stopped_on, :label=>"Au"
-    t.column :name, :through=>:proposer_zone, :url=>{:controller=>:zones, :action=>:show}
-    t.column :name, :through=>:departure_country
-    t.column :name, :through=>:arrival_country
-    t.action :folder_delete, :method=>:delete, :confirm=>:are_you_sure, :if=>'!RECORD.student'
-  end
+#  dyta(:people, :model=>:people, :order=>"family_name, first_name", :conditions=>"(session[:current_promotion_id].blank? ? {} : {:promotion_id=>session[:current_promotion_id]}).merge(:student=>true)", :line_class=>"(RECORD.current? ? 'notice' : '')", :order=>"started_on") do |t|
+#    t.action :folder, :image=>:show
+#    t.column :family_name, :url=>{:controller=>:people, :action=>:show}
+#    t.column :first_name, :url=>{:controller=>:people, :action=>:show}
+#    t.column :code, :through=>:promotion
+#    t.column :started_on, :label=>"Du"
+#    t.column :stopped_on, :label=>"Au"
+#    t.column :name, :through=>:proposer_zone, :url=>{:controller=>:zones, :action=>:show}
+#   t.column :name, :through=>:departure_country
+#    t.column :name, :through=>:arrival_country
+#    t.action :folder_delete, :method=>:delete, :confirm=>:are_you_sure, :if=>'!RECORD.student'
+#  end
 
 
   def index
