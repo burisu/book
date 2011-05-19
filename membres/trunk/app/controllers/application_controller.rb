753f8d6db02e514dffa6372e50383285d3f1fc4f
Brice Texier <burisu@oneiros.fr>
2011-05-19 19:21:30 +0000

    Moves badly located files
    git-svn-id: https://www.rotex1690.org/svn@557 418a08b3-2a41-0410-b009-95f9e35b0d20

--- membres/trunk/app/controllers/application_controller.rb
+++ membres/trunk/app/controllers/application_controller.rb
@@ -38,6 +38,32 @@ class ApplicationController < ActionController::Base
 
   protected
 
+
+  def self.search_conditions(model_name, columns)
+    model = model_name.to_s.classify.constantize
+    columns = [columns] if [String, Symbol].include? columns.class 
+    columns = columns.collect{|k,v| v.collect{|x| "#{k}.#{x}"}} if columns.is_a? Hash
+    columns.flatten!
+    raise Exception.new("Bad columns: "+columns.inspect) unless columns.is_a? Array
+    code = ""
+    code+="c=['true']\n"
+    code+="session[:#{model.name.underscore}_key].to_s.lower.split(/\\s+/).each{|kw| kw='%'+kw+'%';"
+    # This line is incompatible with MySQL...
+    # code+="c[0]+=' AND (#{columns.collect{|x| 'LOWER(CAST('+x.to_s+' AS TEXT)) LIKE ?'}.join(' OR ')})';c+=[#{(['kw']*columns.size).join(',')}]}\n"
+    if ActiveRecord::Base.connection.adapter_name == "MySQL"
+      code+="c[0]+=' AND ("+columns.collect{|x| 'LOWER(CAST('+x.to_s+' AS CHAR)) LIKE ?'}.join(' OR ')+")';\n"
+    else
+      code+="c[0]+=' AND ("+columns.collect{|x| 'LOWER(CAST('+x.to_s+' AS VARCHAR)) LIKE ?'}.join(' OR ')+")';\n"
+    end
+    code+="c+=[#{(['kw']*columns.size).join(',')}]"
+    code+="}\n"
+    code+="c"
+    code
+  end
+
+
+
+
   def authorize()
     @title = 'Bienvenue'
 
@@ -84,6 +110,34 @@ class ApplicationController < ActionController::Base
       redirect_to root_url
       return      
     end    
+  end
+
+
+
+  # Check record existence
+  def find_and_check(model, id, options={})
+    model = model.to_s
+    record = model.classify.constantize.find_by_id(id)
+    if record.nil?
+      flash[:error] = ::I18n.t("general.unavailable.#{model.to_s}", :value=>id)
+      redirect_to_back # :action=>options[:url]||model.pluralize
+    end
+    record
+  end
+
+  def render_form(options={})
+    a = action_name.split '_'
+    @_operation    = options[:operation]||a[-1].to_sym
+    @_operation = :edit if @_operation == :update
+    @_operation = :new if @_operation == :create
+    @_partial = (options[:partial]||a[0..-2].join('_')).to_s
+    @_partial += "_" unless @_partial.blank?
+    @_partial += "form"
+    @_options = options
+    begin
+      render :template=>options[:template]||'shared/form_'+@_operation.to_s
+    rescue ActionController::DoubleRenderError
+    end
   end
 
 end
--- membres/trunk/app/helpers/application_helper.rb
+++ membres/trunk/app/helpers/application_helper.rb
@@ -1,3 +1,4 @@
+# -*- coding: undecided -*-
 module ApplicationHelper
 
   def access?(right=:all)
@@ -20,5 +21,63 @@ module ApplicationHelper
       ''
     end
   end
+
+  def toolbar(input_id)
+    selector = input_id+'_iselector'
+    viewer = input_id+'_viewer'
+    code = ''
+    markups = [
+               ['bold',      ' **',   '** ', 'texte en gras'], 
+               ['italic',    ' //',   '// ', 'texte en italique'], 
+               # ['underline', ' +',    '+ ' , 'texte souligné'], 
+#               ['stroke',    ' -',    '- ' , 'texte rayé'], 
+#               ['sup',       ' ^',    '^ ' , 'texte en exposant'], 
+#               ['sub',       ' ~',    '~ ' , 'texte en indice'], 
+               ['h1',        '\\n===== ', ' =====\\n'   , 'Titre de niveau 1'],
+               ['h2',        '\\n==== ', ' ====\\n'   , 'Titre de niveau 2'],
+               ['h3',        '\\n=== ', ' ===\\n'   , 'Titre de niveau 3'],
+#               ['ul',        '\\n* ', ''   , 'Élément de liste à puce'], 
+#               ['ol',        '\\n# ', ''   , 'Élément de liste numérotée'],
+               ['link',      ' [[http://www.exemple.fr|', ']]' , 'site web'],
+              ]
+    code += content_tag(:div, nil, :id=>selector, :style=>"display:none;", :class=>'iselector')
+    code += link_to_remote(image_tag('buttons/image.png'), {:url=>images_url(:editor=>input_id), :method=>:get,  :update=>"#{selector}", :success=>"$('#{selector}').show()"}, :class=>:tool)
+
+    for markup, start, finish, middle in markups
+      code += link_to_function(image_tag("buttons/#{markup}.png", :alt=>::I18n.t('general.layout.'+markup), :title=>::I18n.t('general.layout.'+markup)), "insertion($('#{input_id}'), '#{start}', '#{finish}', '#{middle}')", :class=>:tool)
+    end
+
+#    markup = 'image'
+#    code += link_to_function(image_tag("buttons/#{markup}.png", :alt=>::I18n.t('general.layout.'+markup), :title=>::I18n.t('general.layout.'+markup)), "openImagePopup('#{url_for(:action=>:media, :id=>input_id)}', 'preview')", :class=>:tool)
+
+    markup = 'show'
+    code += link_to_remote(image_tag('buttons/show.png'), {:url=>preview_articles_url, :with=>"'textile=' + encodeURIComponent($('#{input_id}').value)", :update=>viewer, :success=>"$('#{viewer}').show()", :method=>:get}, :class=>:tool)
+    code += link_to(image_tag('buttons/unknown.png'), help_articles_url, :class=>:tool)
+    code = content_tag(:div, code, :class=>:editor)
+    code = content_tag(:div, nil, :id=>viewer, :style=>"display:none;", :class=>'viewer')+code
+  end
+
+
+
+  def tl(label,field, options={})
+    code  = content_tag('td',label,:class=>"label", :id=>options[:label_id])
+    code += content_tag('td',field,:class=>"value", :id=>options[:value_id])
+    hint  = ''
+    hint += 'Ex&nbsp;: '+options[:example].to_s if options[:example]
+    hint += '<br/>'if hint!='' and options[:hint]
+    hint += 'Astuce&nbsp;: '+options[:hint].to_s if options[:hint]
+    hint += '<br/>'if hint!='' and options[:info]
+    hint += 'Info.&nbsp;: '+options[:info].to_s if options[:info]
+    code += content_tag('td',hint, :class=>"hint", :id=>options[:hint_id])
+    code  = content_tag('tr',code,:class=>cycle("odd","even"))
+    code
+  end
+
+  def tt(label)
+    code  = content_tag(:th, label, :colspan=>3)
+    code  = content_tag(:tr, code)
+    code
+  end
+
 
 end
